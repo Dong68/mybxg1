ae14c76e98c5d6a6e2a655f4a53d712160c85528
xuzhendong <1820945929@qq.com>
2017-09-05 20:04:34 +0800

    完成讲师查看功能

--- public/js/config.js
+++ public/js/config.js
@@ -6,6 +6,13 @@ require.config({
         login:'../js/login',
         cookie:'jquery-cookie/jquery.cookie',
         template:'artTemplate/template-web',
-        teacherlist:'../js/teacher-list'
+        teacherlist:'../js/teacher-list',
+        //引入bootstrap
+        bootstrap:'bootstrap/js/bootstrap.min'
     },
+    shim:{//shim的作用就是把非标准模块转换为标准模块
+        bootstrap: {//把bootstrap转为标准模块
+            deps:['jquery']
+        }
+    }
 });
--- public/js/teacher-list.js
+++ public/js/teacher-list.js
@@ -1,4 +1,4 @@
-define(['jquery','template'], function ($,template) {
+define(['jquery','template','bootstrap'], function ($,template) {
     //请求后台接口，调取后台数据
     $.ajax({
         type:'get',//请求方式
@@ -9,6 +9,26 @@ define(['jquery','template'], function ($,template) {
             //解析数据并且渲染页面
             var html=template('teacherTpl',{list:data.result});
             $('#teacherInfo').html(html);//讲师管理里面的数据
+            //绑定查看的点击事件
+            $('.preveiw').click(function () {
+                //closest()这个方法是离当前元素最近的父元素
+                var tcId=$(this).closest('td').attr('data-tcId');
+                //通过接口调用后台数据
+                $.ajax({
+                   type:'get',
+                   url:'/api/teacher/view',
+                    data:{tc_id:tcId},
+                    dataType:'json',
+                    success: function (data) {
+                        //解析数据渲染页面
+                        var html=template('modalTpl',data.result);
+                        $('#modalInfo').html(html);
+                        //显示弹出模态框
+                        $('#teacherModal').modal();
+                    }
+                });
+            });
         }
-    })
+    });
+
 });
